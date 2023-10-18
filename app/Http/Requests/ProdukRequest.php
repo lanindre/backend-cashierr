4913463519f16f66e87be728eace926158fcc122
Wulan Indah Restiani <anjaimabarwkwk326@gmail.com>
2023-10-18 20:31:20 +0700

    build new nextjs project for cashier app

---
+++ app/Http/Requests/ProdukRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ProdukRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'nama_produk' => 'required'
+        ];
+    }
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/category', CategoryController::class);
+Route::apiResource('/produk', ProdukController::class);
